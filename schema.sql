8f9427ea8e5116a0ede1ce78e5092a03b8aaeccd
Trevor Bruce <tbnyk03@gmail.com>
2021-06-08 14:27:24 -0600

    Create SQL Tables

---
+++ schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employee_sql;
+CREATE DATABASE employee_sql;
+
+USE employee_sql;
+
+CREATE TABLE Departments (
+    id INT NOT NULL,
+    name VARCHAR(30) NUll,
+    PRIMARY KEY (id)
+);
+    
+CREATE TABLE Roles (
+    id INT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
+);
+
+SELECT * FROM Departments;
+SELECT * FROM Roles;
+SELECT * FROM Employee;
